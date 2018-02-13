fa90a07f68553136657e0a2cb13f311e33fa1470
Dario Campagna <campagna@esteco.com>
2018-02-13 12:01:07 +0000

    Removed obsolete code

--- src/prime_factors.cpp
+++ src/prime_factors.cpp
@@ -1,10 +1,9 @@
 #include "prime_factors.h"
 
 std::vector<int> PrimeFactors::of(int number) {
+    std::vector<int> factors;
     if (number == 2){
-        std::vector<int> factors;
         factors.push_back(2);
-        return factors;
     }
-    return std::vector<int>();
+    return factors;
 }
