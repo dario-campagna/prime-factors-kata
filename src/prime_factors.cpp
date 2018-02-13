9f2ec74fce973c80342d266eb2e87458ddd9bb8e
Dario Campagna <campagna@esteco.com>
2018-02-13 12:00:23 +0000

    We now compute prime factors of 2

--- src/prime_factors.cpp
+++ src/prime_factors.cpp
@@ -1,5 +1,10 @@
 #include "prime_factors.h"
 
 std::vector<int> PrimeFactors::of(int number) {
+    if (number == 2){
+        std::vector<int> factors;
+        factors.push_back(2);
+        return factors;
+    }
     return std::vector<int>();
 }
--- test/compute_prime_factors.cpp
+++ test/compute_prime_factors.cpp
@@ -2,5 +2,11 @@
 #include "../src/prime_factors.h"
 
 TEST(ComputePrimeFactors, OneHasNoPrimeFactors) {
-    ASSERT_EQ(std::vector<int>(), PrimeFactors::of(1));
+    ASSERT_EQ(0, PrimeFactors::of(1).size());
+}
+
+TEST(ComputePrimeFactors, TwoPrimeFactors) {
+    const std::vector<int> factors = PrimeFactors::of(2);
+    ASSERT_EQ(1, factors.size());
+    ASSERT_EQ(2, factors[0]);
 }
