68a8a1da5e45f5f4878cfc08d84b7aca79b1ab9b
Dario Campagna <campagna@esteco.com>
2018-02-13 12:09:58 +0000

    We now compute prime factors of 4

--- src/prime_factors.cpp
+++ src/prime_factors.cpp
@@ -6,6 +6,9 @@ std::vector<int> PrimeFactors::of(int number) {
         factors.push_back(2);
     } else if (number == 3){
         factors.push_back(3);
+    } else if (number == 4) {
+        factors.push_back(2);
+        factors.push_back(2);
     }
     return factors;
 }
--- test/compute_prime_factors.cpp
+++ test/compute_prime_factors.cpp
@@ -1,18 +1,25 @@
 #include "gtest/gtest.h"
 #include "../src/prime_factors.h"
 
+void ASSERT_VECTOR_EQ(std::vector<int> expected, std::vector<int> actual){
+    ASSERT_EQ(expected.size(), actual.size()) << "vectors differ in size" << std::endl;
+    for(int i=0; i < expected.size(); i++){
+        ASSERT_EQ(expected[i], actual[i]) << "vectors differ at position " << i << std::endl;
+    }
+}
+
 TEST(ComputePrimeFactors, OneHasNoPrimeFactors) {
     ASSERT_EQ(0, PrimeFactors::of(1).size());
 }
 
 TEST(ComputePrimeFactors, TwoPrimeFactors) {
-    const std::vector<int> factors = PrimeFactors::of(2);
-    ASSERT_EQ(1, factors.size());
-    ASSERT_EQ(2, factors[0]);
+    ASSERT_VECTOR_EQ({2}, PrimeFactors::of(2));
 }
 
 TEST(ComputePrimeFactors, ThreePrimeFactors) {
-    const std::vector<int> factors = PrimeFactors::of(3);
-    ASSERT_EQ(1, factors.size());
-    ASSERT_EQ(3, factors[0]);
+    ASSERT_VECTOR_EQ({3}, PrimeFactors::of(3));
+}
+
+TEST(ComputePrimeFactors, FourPrimeFactors) {
+    ASSERT_VECTOR_EQ({2,2}, PrimeFactors::of(4));
 }
