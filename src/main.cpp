e86c1d6548e1c7aa0fd6d89b3904cb3b9b37fec0
Dario Campagna <campagna@esteco.com>
2018-02-13 15:45:06 +0000

    Main uses PrimeFactors struct

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include "prime_factors.h"
+
+int inputNumber() {
+    int number;
+    std::cout << "Enter a number (>= 2): ";
+    while (!(std::cin >> number) || number < 2) {
+        std::cout << "Please enter a number >= 2" << std::endl;
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');
+    }
+    return number;
+}
 
 int main() {
-	std::cout << "Hello World!" << std::endl;
-	return 0;
+    int number = inputNumber();
+    std::vector<int> factors = PrimeFactors::of(number);
+    std::cout << "The prime factors of " << number << " are:" << std::endl;
+    std::cout << factors[0];
+    for (int i = 1; i < factors.size(); i++) {
+        std::cout << ", " << factors[i];
+    }
+    return 0;
 }
